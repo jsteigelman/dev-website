0ac4209dce1a3353ece04ac725709cfce0f8d4ab
Joey Steigelman <joey.steigelman@gmail.com>
2022-02-08 14:06:01 -0500

    added drawer for about section

---
+++ js/about.js
@@ -0,0 +1,9 @@
+const openAboutDrawer = () => {
+  console.log('the drawer has been opened!')
+  document.querySelector('.aboutDrawer').style.display = 'block'
+}
+
+const closeAboutDrawer = () => {
+  console.log('the drawer has been closed!')
+  document.querySelector('.aboutDrawer').style.display = 'none'
+}
