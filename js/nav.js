d681c520321fc4fbc5b2ecb5e8efda705d44f6c2
Joey Steigelman <joey.steigelman@gmail.com>
2022-02-08 17:34:16 -0500

    updated navbar ui on mobile

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,12 @@
 const toggleNav = () => {
   const nav = document.querySelector('ul')
+  const backgroundLayer = document.querySelector('.bgLayer')
 
   if (nav.style.display === 'none') {
     nav.style.display = 'flex'
+    backgroundLayer.style.display = 'block'
   } else {
     nav.style.display = 'none'
+    backgroundLayer.style.display = 'none'
   }
 }
